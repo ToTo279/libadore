a21da965ae998fe39ffa763053c278f35998cb19
toto279 <tobiasgogo@hotmail.de>
2023-08-01 14:01:15 +0200

    modified:   libadore/adore/apps/include/adore/apps/graph_search.h

--- libadore/adore/apps/include/adore/apps/graph_search.h
+++ libadore/adore/apps/include/adore/apps/graph_search.h
@@ -113,55 +113,211 @@ namespace apps
 
 
         }
-        void update()
+        void setConstPenalty(double value)
         {
-        //--------------------------
-               
-       
-        //-------------------------
+            const_penalty_ = value;
+        }
+        void setSpeedScale(double value)
+        {
+            nominal_planner_->setSpeedScale(value);
+        }
 
-       //adore::fun::VornoiDiagram::update(figure);
-       
-            StartPose_subscreiber= node_->subscribe<geometry_msgs::Pose>("SIM/StartPose",1,&adore_if_ros_scheduling::GraphSearch::receiveStartPose,this);
-            EndPose_subscreiber= node_->subscribe<geometry_msgs::Pose>("SIM/EndPose",1,&adore_if_ros_scheduling::GraphSearch::receiveEndPose,this);
-            
-            while(iteration<2 && validStart && validEnd)
+        void setStopPoint(int value)
+        {
+        if(value<0)
             {
-
-                std::cout<<"\nITERATION: "<<iteration;
-                start = std::chrono::system_clock::now();
-                time1 = 0.0;
-                time2 = 0.0;
-                   
-                //std::cout<<"\n"<<   cco->offlineCollisionTable.size()<<"\t"<<cco->offlineCollisionTable[0].size1()<<"\t"<<cco->offlineCollisionTable[0].size2();   
-                h_A_star->plan(&NH_GRID,&OG, cco, &Start,&End,HeadingResolution,1000, vehicleWidth, vehicleLength ,figure3,figure4,figure5);            
-                end = std::chrono::system_clock::now(); 
-                time1 += std::chrono::duration_cast<std::chrono::microseconds>(end-start).count();   
-                time2 += std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count(); 
-                avg_time += time1;
-                std::cout << "Elapsed time in microseconds (profiler) : "<< time1  << "\n";  
-                //std::cout << "Elapsed time in milliseconds (profiler) : "<< time2  << "\n";    
-                std::cout << "Average time                 (profiler) : "<< (avg_time/iteration)/1000  << "\n";  
-                iteration++;          
-                
+            nominal_planner_->setConflictSet(nullptr);
+            }
+            else
+            {
+            nominal_planner_->setConflictSet(&conflicts_);
             }
 
         }
-        /*void receiveStartPose(geometry_msgs::Pose msg)
+
+        /**
+        * @brief update data, views and recompute maneuver
+        * 
+        */
+        virtual void computeTrajectory(const adore::fun::PlanningRequest& planning_request, adore::fun::PlanningResult& planning_result) override
         {
-                    double r,p,y;
-                    tf::Matrix3x3(tf::Quaternion(msg.orientation.x,msg.orientation.y,msg.orientation.z,msg.orientation.w)).getRPY(r,p,y);
-                    validStart = Start.setPosition(msg.position.x,msg.position.y,y,Width,Length,Depth,adore::mad::CoordinateConversion::DegToRad(HeadingResolution), figure3);
-                    //Start.print();
-        }  
-        void receiveEndPose(geometry_msgs::Pose msg)
-        {
-                    double r,p,y;
-                    tf::Matrix3x3(tf::Quaternion(msg.orientation.x,msg.orientation.y,msg.orientation.z,msg.orientation.w)).getRPY(r,p,y);
-                    validEnd = End.setPosition(msg.position.x,msg.position.y,y,Width,Length,Depth, adore::mad::CoordinateConversion::DegToRad(HeadingResolution),  figure3);
-                    //End.print();
-        } */             
-    };
+            //document planner result
+            planning_result.id = id_; // in lfbehavior, there is only one maneuver
+            planning_result.name = plannerName_;
+            planning_result.maneuver_type = adore::fun::PlanningResult::NOMINAL_DRIVING;
+            planning_result.iteration = planning_request.iteration;
+            planning_result.nominal_maneuver_valid = false;
+            planning_result.combined_maneuver_valid = false;
+
+            three_lanes_.update();
+            auto current = three_lanes_.getCurrentLane();
+            ngo_.update();
+            connectionSet_.update();
+            connectionsOnLane_->update();
+            checkPointSet_.update();
+            checkPointsOnLane_->update();
+            conflicts_.update();
+
+
+            if(!current->isValid())
+            {
+                planning_result.status_string = "current lane invalid";
+                return;
+            }
+
+            prediction_.update();
+
+            auto x0=planning_request.initial_state.toMotionState();
+            nominal_planner_->compute(x0);
+            if(!nominal_planner_->hasValidPlan())
+            {
+            planning_result.status_string = "nominal maneuver planning failed, "+ nominal_planner_->getStatus();
+            return;
+            }
+
+            nominal_planner_->getSetPointRequest()->copyTo(planning_result.nominal_maneuver);
+            planning_result.nominal_maneuver_valid = true;
+            auto x0em = nominal_planner_->getSetPointRequest()->interpolateSetPoint(planning_request.t_emergency_start,pvehicle_);
+            //emergency_planner_->setJMax(-pEmergencyOperation_->getEmergencyManeuverJMin());
+            //emergency_planner_->setTStall(pEmergencyOperation_->getEmergencyManeuverTStall());
+
+            // adore::fun::RestartEffort re;  //test whether trajectory is long enough to justify restarting from ~0
+            // if(!re.isValid(planning_result.nominal_maneuver))
+            // {
+            //   planning_result.status_string = "not restarting, maneuver too short";
+            //   return;
+            // }
+
+            //if emergencyManeuverAMax and AMin are different, compute three maneuvers for {amin, (amin+amax)/2, amax}
+            //otherwise compute just one maneuver for amin
+            /*std::vector<double> emergency_acceleration;
+            emergency_acceleration.push_back(pEmergencyOperation_->getEmergencyManeuverAMin());
+            if(  pEmergencyOperation_->getEmergencyManeuverAMax()
+            - pEmergencyOperation_->getEmergencyManeuverAMin() > 0.01 )
+            {
+            emergency_acceleration.push_back((pEmergencyOperation_->getEmergencyManeuverAMin()
+                                            + pEmergencyOperation_->getEmergencyManeuverAMax())*0.5);
+            emergency_acceleration.push_back(pEmergencyOperation_->getEmergencyManeuverAMax());
+            }
+
+            bool collision_detection_passed = false;
+
+            for(double a_em:emergency_acceleration)
+            {
+            emergency_planner_->setAStall(a_em);
+            emergency_planner_->setAMin(a_em);
+            emergency_planner_->compute(x0em.toMotionState());
+
+            if(!emergency_planner_->hasValidPlan() ) 
+            {
+                continue;
+            }*/
+
+            planning_result.combined_maneuver.setPoints.clear();
+            nominal_planner_->getSetPointRequest()->copyTo(planning_result.combined_maneuver,0);
+            planning_result.combined_maneuver.removeAfter(planning_request.t_emergency_start);
+            planning_result.combined_maneuver.setPoints.back().tEnd = planning_request.t_emergency_start;
+            //emergency_planner_->getSetPointRequest()->copyTo(planning_result.combined_maneuver,1);
+
+
+            if(collision_detection_.isValid(planning_result.combined_maneuver))
+            {
+                collision_detection_passed = true;
+                break;
+            }
+            //}
+
+
+            /*if(!emergency_planner_->hasValidPlan() ) 
+            {
+            planning_result.status_string += "emergency maneuver planning failed";
+            return;
+            }*/
+
+            double front_buffer_space = pTacticalPlanner_->getCollisionDetectionFrontBufferSpace();
+            double lateral_precision = pTacticalPlanner_->getCollisionDetectionLateralPrecision();
+            adore::fun::SetPointRequestSwath spr_swath(
+                pvehicle_->get_a()+pvehicle_->get_b()+pvehicle_->get_c()+pvehicle_->get_d()+front_buffer_space,
+                pvehicle_->get_bodyWidth(),
+                pvehicle_->get_d(),//spr reference point at rear axle
+                lateral_precision);
+            spr_swath.setLonError(pTacticalPlanner_->getCollisionDetectionLongitudinalError());
+            spr_swath.setLatError(pTacticalPlanner_->getCollisionDetectionLateralError());
+            spr_swath.setDuration(ppred_->get_roadbased_prediction_duration());
+            spr_swath.append_cylinder_swath_linear(planning_result.combined_maneuver,planning_result.combined_maneuver_swath,true);
+            spr_swath.setAccelerationErrorSlow(pTacticalPlanner_->getNominalSwathAccelerationError());
+            spr_swath.append_cylinder_swath_linear(planning_result.nominal_maneuver,planning_result.nominal_maneuver_swath,true);
+
+            if(!collision_detection_passed)
+            {
+            planning_result.status_string = "collision detected for combined maneuver";    
+            return;          
+            }
+
+            planning_result.combined_maneuver.cropAfterFirstStop(0.3);
+            if(planning_result.combined_maneuver.setPoints.size()==0)
+            {
+            planning_result.status_string = "stopping";
+            return;
+            }
+
+            planning_result.combined_maneuver_valid = true;
+            int laneid = 0;
+            adore::fun::SPRNavigationCostOnLane navcostOnLane(&three_lanes_,pTacticalPlanner_,laneid);
+            planning_result.objective_values.insert({navcostOnLane.getName(),
+                                                    navcostOnLane.getCost(planning_result.nominal_maneuver)});
+
+            adore::fun::SPRNormalizedNavigationCost navcostNormalized(&three_lanes_,pTacticalPlanner_,laneid);
+            planning_result.objective_values.insert({navcostNormalized.getName(),
+                                                    navcostNormalized.getCost(planning_result.nominal_maneuver)});
+
+            adore::fun::SPRLongitudinalAcceleration2Cost lacccost;
+            planning_result.objective_values.insert({lacccost.getName(),
+                                                    lacccost.getCost(planning_result.nominal_maneuver)});
+
+            adore::fun::SPRLongitudinalJerk2Cost ljerkcost;
+            planning_result.objective_values.insert({ljerkcost.getName(),
+                                                    ljerkcost.getCost(planning_result.nominal_maneuver)});
+
+            adore::fun::SPRAverageProgressLoss progressLoss(pTacticalPlanner_);
+            planning_result.objective_values.insert({progressLoss.getName(),
+                                                    progressLoss.getCost(planning_result.nominal_maneuver)});
+
+            planning_result.objective_values.insert({"const_penalty",const_penalty_});
+
+            //ttc cost
+            planning_result.objective_values.insert({ttcCost_.getName(),
+                                                    ttcCost_.getCost(planning_result.nominal_maneuver)});
+
+
+            switch(pTacticalPlanner_->getCoercionPreventionStrategy())
+            {
+            case 0://turned off
+                {
+                planning_result.objective_values.insert({coercion_detection_.getName(),0.0});
+                }
+                break;
+            case 1://encode as objective value
+                {
+                planning_result.objective_values.insert({coercion_detection_.getName(),
+                                                            coercion_detection_.isValid(planning_result.nominal_maneuver)
+                                                            ? 0.0 : 1.0});
+                }
+                break;
+            case 2://encode as constraint
+                {
+                bool coercion_detection_passed = coercion_detection_.isValid(planning_result.nominal_maneuver);
+                planning_result.combined_maneuver_valid = coercion_detection_passed && planning_result.combined_maneuver_valid;
+                if(!coercion_detection_passed)
+                {
+                    planning_result.status_string = "coercion detected for nominal maneuver";    
+                }
+                }
+                break;
+            }
+        }
+    }
 }
 }
 
+    
